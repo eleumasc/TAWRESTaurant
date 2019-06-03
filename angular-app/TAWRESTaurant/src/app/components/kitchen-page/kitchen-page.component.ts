6da778cc10ab635d60d37ea6ba737ec70326a78f
AlviseB <37024233+AlviseB@users.noreply.github.com>
2019-06-03 23:36:22 +0200

    non so un cazzo di angular, ma adesso non lancia più errori

--- angular-app/TAWRESTaurant/src/app/components/kitchen-page/kitchen-page.component.ts
+++ angular-app/TAWRESTaurant/src/app/components/kitchen-page/kitchen-page.component.ts
@@ -1,48 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { OrdersService } from 'src/app/services/orders.service';
-import { ActivatedRoute } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
-import { User } from 'src/app/models/User';
-import { OrderKind, Order } from 'src/app/models/Order';
-import { Table } from 'src/app/models/Table';
+import { Component, OnInit } from "@angular/core";
+import { OrdersService } from "src/app/services/orders.service";
+import { ActivatedRoute } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
+import { User } from "src/app/models/User";
+import { OrderKind, Order } from "src/app/models/Order";
+import { Table } from "src/app/models/Table";
 
 @Component({
-  selector: 'app-kitchen-page',
-  templateUrl: './kitchen-page.component.html',
-  styleUrls: ['./kitchen-page.component.css']
+  selector: "app-kitchen-page",
+  templateUrl: "./kitchen-page.component.html",
+  styleUrls: ["./kitchen-page.component.css"]
 })
-
 export class KitchenPageComponent implements OnInit {
-
   auth: User;
-  kind: OrderKind;
-  ref: string;
-  status: string;
-  user: string;
 
-  constructor(private authService: AuthService,
+  constructor(
+    private authService: AuthService,
     private route: ActivatedRoute,
-    private orderService: OrdersService) { }
+    private orderService: OrdersService
+  ) {}
 
   ngOnInit() {
     this.auth = this.authService.getUser();
-    this.route.data.subscribe((data) => {
-      this.kind = data.kind
-      if (this.kind === OrderKind.FoodOrder) {
-        this.user = 'cook';
-        this.ref = 'food';
-        this.status = 'foodOrderStatus';
-      }
-      else {
-        this.user = 'barman';
-        this.ref = 'beverage';
-        this.status = 'beverageOrderStatus';
-      }
-    });
   }
 
-  prepare(table:Table, order: Order){
-    this.orderService.assignOrder(table,order).then()
+  prepare(table: Table, order: Order) {
+    this.orderService.assignOrder(table, order).then();
   }
-
 }
